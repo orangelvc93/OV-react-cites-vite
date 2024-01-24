c6379c066e14d967b6ae144c2a23ab7ffdfc8adc
Orangel Valdespino <orangelvc93@gmail.com>
2024-01-24 09:59:53 -0500

    Ajustando las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Footer from "./components/Footer";
 import Form from "./components/Form";
 import Header from "./components/Header";
-import PatientList from "./components/patientList";
+import PatientList from "./components/PatientList";
 
 function App() {
 	return (
